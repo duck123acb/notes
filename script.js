a4629419e072e41f10ecd4a17ba54fa8f0b802fc
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-08-08 16:57:40 -0600

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const textbox = document.getElementById("text");
-const downloadBtn = document.getElementById("download");
 
-function download() {
-	var myFile = new File([textbox.value], "notes.txt", {type: "text/plain;charset=utf-8"});
-    saveAs(myFile);
-}
-
-function upload() {
-	
-}
-
-downloadBtn.addEventListener("click", function() {
-	download();
-});
