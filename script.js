e9c70c172ac3742e7f610f6e1d3027c6d5f77c5a
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-08-08 18:18:38 -0600

    add uploading

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ function download() {
 }
 
 function upload() {
-	
+    let fr = new FileReader();
+	fr.addEventListener('load', (event) => {
+    	textbox.value = event.target.result;
+  	});
 }
+
+uploadBtn.addEventListener("change", function () {
+	upload();
+});
 
 downloadBtn.addEventListener("click", function() {
 	download();
