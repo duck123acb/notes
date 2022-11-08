705e9ed658d80c54b21c8b6be4607e8faa4892ba
duckabc123 <dan.ramanan@gmail.com>
2022-11-08 14:19:53 -0700

    IT MIGHT WORKgit add .

--- script.js
+++ script.js
@@ -30,8 +30,7 @@ function underline() {
 }
 
 function download() {
-	let notesFile = new File([textbox.innerText], "notes.txt", {type: "text/plain;charset=utf-8"});
-	// saveAs(notesFile);
+
 	saveAs(new File([textbox.innerHTML], {type:"text/plain;charset=utf-8"}), "hi.xhtml")
 }
 
