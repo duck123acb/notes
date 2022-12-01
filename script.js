247f9f64cff7c84cb746cc904865be2c9fdd1fa5
Danush Ramanan <dan.ramanan@gmail.com>
2022-12-01 07:25:52 -0700

    when hit tab key, insert tab

--- script.js
+++ script.js
@@ -2,26 +2,30 @@ const textbox = document.getElementById("text");
 const downloadBtn = document.getElementById("download");
 const uploadBtn = document.getElementById("upload");
 
-// setup
-textbox.contentEditable = true;
-
 function download() {
-	saveAs(new File([textbox.innerHTML], {type:"text/plain;charset=utf-8"}), "notes.ducknote")
+	saveAs(new File([textbox.value], {type:"text/plain;charset=utf-8"}), "notes.ducknote")
 }
 
 function upload() {
 	let fr = new FileReader();
 	fr.addEventListener('load', (event) => {
-		textbox.innerText = event.target.result;
+		textbox.value = event.target.result;
   	});
 	fr.readAsText(uploadBtn.files[0]);
 }
 
 uploadBtn.addEventListener("change", upload);
 downloadBtn.addEventListener("click", download);
-addEventListener("beforeunload", function (e) {
+textbox.addEventListener("keydown", function(event) {
+	if (event.key === "Tab") {
+		event.preventDefault();
+		const [startPos, endPos] = [textbox.selectionStart, textbox.selectionEnd];
+		textbox.setRangeText("\t", startPos, endPos, "end");
+	}
+});
+addEventListener("beforeunload", function(event) {
     const confirmationMessage = 'It looks like you have been editing something. ' + 'If you leave before saving, your changes will be lost.';
 
-    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    (event || window.event).returnValue = confirmationMessage; //Gecko + IE
     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
 });
