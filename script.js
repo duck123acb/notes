98620a051b71f4b3455ea0dbc86df73e05d65191
Danush Ramanan <dan.ramanan@gmail.com>
2022-12-06 07:13:04 -0700

    add calculations, still need to check some more

--- script.js
+++ script.js
@@ -23,6 +23,69 @@ function insertTab() {
 	textbox.setRangeText("\t", startPosition, endPosition, "end");
 }
 
+function calculate() {
+	let [startPosition, endPosition] = getCursorPosition();
+	const selectedText = window.getSelection().toString();
+	const [numberOneString, operand, numberTwoString] = selectedText.split(" ");
+	let numberOne, numberTwo;
+
+	// TODO: something is going wrong with the checking for NaN
+	try {
+		numberOne = parseFloat(numberOneString); // does a weird thing where if there is a string behind the number, it will shave that string off
+		numberTwo = parseFloat(numberTwoString);
+	} catch (error) {
+		alert("Please enter in a correct statement, if you need help, please review the calculations help");
+		return;
+	}
+
+	if (numberOne === NaN || numberTwo === NaN) {
+		alert("Please enter in a correct statement, if you need help, please review the calculations help");
+		return;
+	}
+	if (numberOne === undefined || numberTwo === undefined || operand === undefined) {
+		alert("Please enter in a correct statement, if you need help, please review the calculations help");
+		return;
+	}
+
+	let calculation = 0;
+
+	console.log(numberOne, numberTwo, operand)
+
+	switch (operand) {
+		case "+":
+			calculation = numberOne + numberTwo;
+			break;
+
+		case "-":
+			calculation = numberOne - numberTwo;
+			break;
+
+		case "*":
+			calculation = numberOne * numberTwo;
+			break;
+
+		case "/":
+			calculation = numberOne / numberTwo;
+			break;
+
+		case "**":
+			calculation = numberOne ** numberTwo;
+			break;
+
+		case "//":
+			calculation = Math.sqrt(numberOne,  numberTwo);
+			break;
+	
+		default:
+			alert("Please enter in a correct statement, if you need help, please review the calculations help");
+			return;
+	}
+
+	textbox.setSelectionRange(endPosition + 1, endPosition + 1);
+	[startPosition, endPosition] = getCursorPosition();
+	textbox.setRangeText(` = ${calculation}`, startPosition, endPosition, "end");
+}
+
 uploadBtn.addEventListener("change", upload);
 downloadBtn.addEventListener("click", download);
 textbox.addEventListener("keydown", function(event) {
@@ -31,9 +94,9 @@ textbox.addEventListener("keydown", function(event) {
 		insertTab();
 	}
 });
-addEventListener("beforeunload", function(event) {
-    const confirmationMessage = 'It looks like you have been editing something. ' + 'If you leave before saving, your changes will be lost.';
+// addEventListener("beforeunload", function(event) {
+//     const confirmationMessage = 'It looks like you have been editing something. ' + 'If you leave before saving, your changes will be lost.';
 
-    (event || window.event).returnValue = confirmationMessage; //Gecko + IE
-    return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-});
+//     (event || window.event).returnValue = confirmationMessage; //Gecko + IE
+//     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+// });
